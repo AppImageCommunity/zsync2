AppImageCommunity/zsync2

commit a86c5942a3bceb71e668e2f9924774dde15d7379

Remove more legacy code
Also refactor some of the global static variables we want to get rid of.
See last commit for some details.

src/legacy_http.c
@@ -33,16 +33,7 @@
 #include <time.h>
 #include <curl/curl.h>
 
-#ifndef HAVE_GETADDRINFO
-#include "getaddrinfo.h"
-#endif
-
-#ifdef WITH_DMALLOC
-# include <dmalloc.h>
-#endif
-
 #include "legacy_http.h"
-#include "legacy_url.h"
 #include "legacy_progress.h"
 #include "format_string.h"
 
@@ -69,17 +60,13 @@ struct http_file
 
 typedef struct http_file HTTP_FILE;
 
-/* global curl handle */
-CURLM *multi_handle;
-
-char *cookie;
-
 struct range_fetch {
     /* URL to retrieve from, host:port, auth header */
     char *url;
     HTTP_FILE *file;
     char *boundary; /* If we're in the middle of reading a mime/multipart
                      * response, this is the boundary string. */
+    CURLM *multi_handle;
 
     /* State for block currently being read */
     size_t block_left;  /* non-zero if we're in the middle of reading a block */
@@ -280,8 +267,8 @@ HTTP_FILE *http_fetch_ranges(struct range_fetch* rf)
 {
     HTTP_FILE *file;
 
-    if(!multi_handle){
-        multi_handle = curl_multi_init();
+    if(!rf->multi_handle){
+        rf->multi_handle = curl_multi_init();
     }
 
     if(!rf->file) {
@@ -308,11 +295,11 @@ HTTP_FILE *http_fetch_ranges(struct range_fetch* rf)
     /* we still process the headers ourselves so we can get range information */
     curl_easy_setopt(file->handle.curl, CURLOPT_HEADER, 1L);
     curl_easy_setopt(file->handle.curl, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_multi_add_handle(multi_handle, file->handle.curl);
+    curl_multi_add_handle(rf->multi_handle, file->handle.curl);
     rf->file = file;
 
     http_load_ranges(rf);
-    curl_multi_perform(multi_handle, &rf->file->still_running);
+    curl_multi_perform(rf->multi_handle, &rf->file->still_running);
 
     return rf->file;
 }
@@ -322,7 +309,7 @@ HTTP_FILE *http_fetch_ranges(struct range_fetch* rf)
  *
  * Closes the connection, cleans up curl.
  */
-int http_fclose(HTTP_FILE *file)
+int http_fclose(HTTP_FILE *file, CURLM* multi_handle)
 {
     curl_multi_remove_handle(multi_handle, file->handle.curl);
     curl_easy_cleanup(file->handle.curl);
@@ -347,7 +334,7 @@ int http_feof(HTTP_FILE *file)
 }
 
 
-static int fill_buffer(HTTP_FILE *file, size_t want)
+static int fill_buffer(HTTP_FILE *file, size_t want, CURLM* multi_handle)
 {
     fd_set fdread;
     fd_set fdwrite;
@@ -427,11 +414,11 @@ static int use_buffer(HTTP_FILE *file, int want)
  *
  * Reads bytes from a HTTP_FILE.
  */
-size_t http_fread(void *ptr, size_t size, size_t nmemb, HTTP_FILE *file)
+size_t http_fread(void *ptr, size_t size, size_t nmemb, HTTP_FILE *file, struct range_fetch *rf)
 {
     size_t want;
     want = nmemb * size;
-    fill_buffer(file, want);
+    fill_buffer(file, want, rf);
 
     if(!file->buffer_pos){
         /* nothing read, nothing in buffer */
@@ -464,7 +451,7 @@ char *rfgets(char *ptr, size_t size, struct range_fetch *rf)
     size_t want = size - 1;/* always need to leave room for zero termination */
     size_t loop;
     HTTP_FILE *file = rf->file;
-    fill_buffer(file, want);
+    fill_buffer(file, want, rf);
 
     /* check if theres data in the buffer - if not fill either errored or
      * EOF */
@@ -520,6 +507,7 @@ struct range_fetch *range_fetch_start(const char *orig_url) {
     rf->file = NULL;                        /* http file not open */
     rf->ranges_todo = NULL;             /* And no ranges given yet */
     rf->nranges = rf->rangesdone = 0;
+    rf->multi_handle = NULL;
 
     return rf;
 }
@@ -821,7 +809,7 @@ int get_range_block(struct range_fetch *rf, off_t * offset, unsigned char *data,
         bytes_to_request = dlen;
     }
 
-    bytes_to_caller = http_fread(data, 1, bytes_to_request, rf->file);
+    bytes_to_caller = http_fread(data, 1, bytes_to_request, rf->file, rf);
 
     /* update internal stats of how many blocks are left,
        file offset, and bytes downloaded. */
@@ -842,7 +830,7 @@ off_t range_fetch_bytes_down(const struct range_fetch * rf) {
 void range_fetch_end(struct range_fetch *rf) {
     /* this will clean up the file, buffer, and close the connection */
     if (rf->file != NULL)
-        http_fclose(rf->file);
+        http_fclose(rf->file, rf->multi_handle);
 
     free(rf->ranges_todo);
     free(rf->boundary);

src/CMakeLists.txt
@@ -20,7 +20,7 @@ configure_file(${CMAKE_CURRENT_SOURCE_DIR}/config.h.in ${CMAKE_CURRENT_BINARY_DI
 include_directories(${CMAKE_CURRENT_BINARY_DIR})
 
 set(ZSYNC2_DEPS ${CPR_LIBRARIES} libzsync)
-set(ZSYNC2_SRCS zsclient.cpp legacy_http.c legacy_url.c legacy_progress.c)
+set(ZSYNC2_SRCS zsclient.cpp legacy_http.c legacy_progress.c)
 
 # core library
 add_library(libzsync2 ${ZSYNC2_SRCS})

src/legacy_http.h
@@ -16,10 +16,6 @@
 
 #pragma once
 
-int set_proxy_from_string(const char* s);
-
-FILE* http_get(const char* orig_url, char** track_referer, const char* tfname);
-
 struct range_fetch;
 
 struct range_fetch* range_fetch_start(const char* orig_url);
@@ -27,8 +23,3 @@ void range_fetch_addranges(struct range_fetch* rf, off_t* ranges, int nranges);
 int get_range_block(struct range_fetch* rf, off_t* offset, unsigned char* data, size_t dlen);
 off_t range_fetch_bytes_down(const struct range_fetch* rf);
 void range_fetch_end(struct range_fetch* rf);
-
-void add_auth(char* host, char* user, char* pass);
-
-/* base64.c */
-char* base64(const char*);

src/legacy_progress.c
@@ -13,18 +13,10 @@
  *   COPYING file for details.
  */
 
-#include "zsglobal.h"
-
 #include <stdio.h>
 #include <time.h>
 
-#ifdef WITH_DMALLOC
-# include <dmalloc.h>
-#endif
-
 #include "legacy_progress.h"
-
-int no_progress;
 
 /* progbar(chars, percent)
  * (Re)print progress bar with chars out of 20 shown and followed by the given

src/legacy_url.c
@@ -1,164 +1,0 @@
-/*
- *   zsync - client side rsync over http
- *   Copyright (C) 2004,2005,2009 Colin Phipps <cph@moria.org.uk>
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the Artistic License v2 (see the accompanying 
- *   file COPYING for the full license terms), or, at your option, any later 
- *   version of the same license.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   COPYING file for details.
- */
-
-/* Very crude URL parsing module */
-
-#include "zsglobal.h"
-
-#include <string.h>
-#include <stdlib.h>
-
-#ifdef WITH_DMALLOC
-# include <dmalloc.h>
-#endif
-
-#include "legacy_url.h"
-
-const char http_scheme[] = { "http://" };
-
-/* path_str = get_http_host_port(url_str, &host, host_len, &port_str)
- * For the given url_str, returns:
- * the hostname, in host[] array provided by caller (up to length host_len)
- * the port (if any) as a malloced string, pointer to this stored in *port_str
- * return value is the path part of the URL, a malloced string.
- * Or return value NULL on failure (host and port could have been written to).
- */
-char *get_http_host_port(const char *url, char *hostn, int hnlen, char **port) {
-    char *p, *q;
-
-    /* Check it's HTTP */
-    if (memcmp(url, http_scheme, strlen(http_scheme)))
-        return NULL;
-
-    q = url + strlen(http_scheme);
-
-    p = strchr(q, ':');
-    if (p) {                    /* if : is after the first /, we have looked too far ahead */
-        char *r = strchr(q, '/');
-        if (r && r < p)
-            p = NULL;
-    }
-    if (!p) {
-        *port = strdup("http");
-        p = strchr(q, '/');
-    }
-
-    if (!p)
-        return NULL;
-
-    if (p - q < hnlen - 1) {
-        memcpy(hostn, q, p - q);
-        hostn[p - q] = 0;
-    }
-
-    if (*p == ':') {
-        size_t l;
-        q = p;
-        p = strchr(p, '/');
-        l = p ? (size_t) (p - q - 1) : strlen(q) - 1;
-        *port = malloc(l + 1);
-        if (!*port)
-            return NULL;
-        memcpy(*port, q + 1, l);
-        (*port)[l] = 0;
-        if (!p)
-            p = strdup("/");
-    }
-    return p;
-}
-
-/* abs_url_str = make_url_absolute(base_str, url_str)
- * Returns an absolute version of url_str relative to base_str, as a malloced
- * string. Or NULL on error. */
-char * __attribute__ ((pure)) make_url_absolute(const char *base, const char *url) {
-    if (is_url_absolute(url))
-        return strdup(url);
-
-    /* Otherwise, we'll need a base URL to get the scheme and host */
-    if (!base)
-        return NULL;
-
-    /* Next, is it a full-path URL? */
-    if (*url == '/') {
-        size_t l;
-        char *newurl;
-
-        /* Find the end of the scheme of the base URL, then the end of the
-         * hostname[:port]/ */
-        const char *p = strchr(base, ':');
-        if (!p || p[1] != '/' || p[2] != '/')
-            return NULL;
-        p = strchr(p + 3, '/');
-        if (!p)    /* We have a http://hostname URL (no trailing / or path) */
-            l = strlen(base);
-        else       /* http://hostname/... , we want just http://hostname */
-            l = p - base;
-
-        /* assert */
-        if (base[l] != '/' && base[l] != '\0')
-            return NULL;
-
-        /* Append full path to the base http://hostname */
-        newurl = malloc(l + strlen(url) + 1);
-        memcpy(newurl, base, l);
-        strcpy(newurl + l, url);
-        return newurl;
-    }
-
-    /* No leading / or scheme - relative path */
-    {
-        int l;
-        char *newurl;
-
-        /* Find the end of the path part of the base URL */
-        const char *q;
-        const char *p = strchr(base, '?');
-        if (!p)
-            p = strchr(base, '#');
-        if (!p)
-            p = base + strlen(base);
-
-        /* Find the last / in the path part */
-        for (q = p; q > base && *q != '/'; q--);
-        if (*q != '/')
-            return NULL;
-
-        /* Take the base URL up to and including the last /, and append the relative URL */
-        l = q - base + 1;
-        newurl = malloc(l + strlen(url) + 1);
-        memcpy(newurl, base, l);
-        strcpy(newurl + l, url);
-        return newurl;
-    }
-}
-
-/* int n = is_url_absolute(url)
- * Returns 0 if the supplied string is not an absolute URL.
- * Returns the number of characters in the URL scheme if it is.
- */
-static const char special[] = { ":/?" };
-
-int __attribute__ ((pure)) is_url_absolute(const char *url) {
-    /* find end of first no-special-URL-characters part of the string */
-    int n = strcspn(url, special);
-
-    /* If the first special character is a :, the start is a URL scheme */
-    if (n > 0 && url[n] == ':')
-        return n;
-
-    /* otherwise, it's a full path or relative path URL, or just a local file
-     * path (caller knows the context) */
-    return 0;
-}

src/legacy_url.h
@@ -1,29 +1,0 @@
-/*
- *   zsync - client side rsync over http
- *   Copyright (C) 2004,2005,2009 Colin Phipps <cph@moria.org.uk>
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the Artistic License v2 (see the accompanying
- *   file COPYING for the full license terms), or, at your option, any later
- *   version of the same license.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   COPYING file for details.
- */
-
-#pragma once
-
-extern const char http_scheme[];
-
-/* Given an HTTP URL, return the path path of the URL as the return value, and
- * return the hostname in the provided buffer (hostn, length hnlen)
- * and, if present, return a (malloced) string buffer containing the port string.
- * Or return NULL if not HTTP or other parsing failure.
- */
-char* get_http_host_port(const char* url, char* hostn, int hnlen, char** port);
-
-char* __attribute__((pure)) make_url_absolute(const char* base, const char* url);
-
-int __attribute__((pure)) is_url_absolute(const char* url);

commit c10cd8f52d4b37de3fc6d2f030d139e1f3d8da84

Fix broken downloads bug introduced in a86c594
CURLM somehow maps to void*, so passing a struct range_fetch*
accidentally didn't trigger a compiler error, but broke the CURL multi
functions. Fixed by passing the right data.

src/legacy_http.c
@@ -418,7 +418,7 @@ size_t http_fread(void *ptr, size_t size, size_t nmemb, HTTP_FILE *file, struct 
 {
     size_t want;
     want = nmemb * size;
-    fill_buffer(file, want, rf);
+    fill_buffer(file, want, rf->multi_handle);
 
     if(!file->buffer_pos){
         /* nothing read, nothing in buffer */
@@ -451,7 +451,7 @@ char *rfgets(char *ptr, size_t size, struct range_fetch *rf)
     size_t want = size - 1;/* always need to leave room for zero termination */
     size_t loop;
     HTTP_FILE *file = rf->file;
-    fill_buffer(file, want, rf);
+    fill_buffer(file, want, rf->multi_handle);
 
     /* check if theres data in the buffer - if not fill either errored or
      * EOF */