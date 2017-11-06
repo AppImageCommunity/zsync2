327c2ca861bfb1221d49196def44bb4f81b97404
TheAssassin <theassassin@users.noreply.github.com>
2017-11-06 21:51:36 +0100

    Add missing flags to CLI interface
    They aren't working yet, so TODO: implement their functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,45 @@
 using namespace std;
 
 int main(const int argc, const char** argv) {
-    args::ArgumentParser parser("zsync2 -- the probably easiest efficient way to update files",
-                                "Brand new C++11 frontend to zsync.");
-    args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
-    args::ValueFlagList<std::string> seedFiles(parser, "seed file", "Use data from this file during update process. "
-                                                                    "Can be specified more than once.", {'I'});
-    args::Flag checkForChanges(parser, "checkForChanges", "Check for changes on the server. Exits with code 1 if the "
-                                                          "file changed on the server, otherwise 0.", {'j'});
-    args::Flag verbose(parser, "verbose", "Switch on verbose mode", {'v'});
-    args::Positional<std::string> pathOrUrl(parser, "path or URL", "Path or URL to .zsync(2) file");
+    args::ArgumentParser parser(
+        "zsync2 -- the probably easiest efficient way to update files",
+        "Brand new C++11 frontend to zsync."
+    );
+
+    args::HelpFlag help(parser, "help", "Display this help menu.", {'h', "help"});
+
+    args::ValueFlagList<string> seedFiles(parser, "path",
+        "Use data from this file during update process. Can be specified more than once.",
+        {'i', "seed-file"}
+    );
+
+    args::Flag httpInsecureMode(parser, "", "Switch to HTTP insecure mode.", {'I', "insecure"});
+
+    args::Flag checkForChanges(parser, "",
+        "Check for changes on the server. Exits with code 1 if the file changed on the server, otherwise 0.",
+        {'j', "check-for-changes"}
+    );
+
+    args::ValueFlag<string> saveZSyncFilePath(parser, "path",
+        "Save copy of .zsync file to given path.",
+        {'k', "copy-zsync-file-to"}
+    );
+
+    args::Flag resolveRedirect(parser, "",
+        "Resolve redirection for given URL and exit.",
+        {'r', "resolve-redirection"}
+    );
+
+    args::ValueFlag<string> outputFilename(parser, "path",
+        "Path to local file which should be created. If not given, file path in .zsync file will be used.",
+        {'o', "output"}
+    );
+
+    args::Flag quietMode(parser, "", "Quiet mode", {'s', 'q', "silent-mode"});
+    args::Flag verbose(parser, "", "Switch on verbose mode", {'v', "verbose"});
+    args::Flag showVersion(parser, "", "Print version and exit", {'V', "version"});
+
+    args::Positional<string> pathOrUrl(parser, "path or URL", "Path or URL to .zsync(2) file");
 
     try {
         parser.ParseCLI(argc, argv);
