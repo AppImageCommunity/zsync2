cdff8beb2fcf41483c74e6dce9ec9262bef5b3c0
Jonathon Fernyhough <jonathon@m2x.dev>
2021-03-05 01:37:33 +0000

    Allow other HTTP protocol versions

--- src/legacy_http.c
+++ src/legacy_http.c
@@ -521,7 +521,7 @@ int range_fetch_read_http_headers(struct range_fetch *rf) {
         }
         if (buf[0] == 0)
             return 0;           /* EOF, caller decides if that's an error */
-        if (memcmp(buf, "HTTP/1", 6) != 0 || (p = strchr(buf, ' ')) == NULL) {
+        if (memcmp(buf, "HTTP/", 5) != 0 || (p = strchr(buf, ' ')) == NULL) {
             log_message("got non-HTTP response '%s'\n", buf);
             return -1;
         }
